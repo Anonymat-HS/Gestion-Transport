b2d4f619511ab9fc664d5ed16b9a03263dea4913
FRANÇOISE Anthonia <hei.anthonia@gmail.com>
2026-07-01 19:52:06 +0300

    feat: add method

--- src/main/java/com/transport/n3/Receptionniste.java
+++ src/main/java/com/transport/n3/Receptionniste.java
@@ -1,15 +1,23 @@
 package com.transport.n3;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Receptionniste extends Employee{
+@Getter
+@Setter
+
+public class Receptionniste extends Employee {
 
     private List<Reservation> listeReservation;
 
     public Receptionniste(String id, String nom, String prenom, String telephone, Sexe sexe, double salaire) {
         super(id, nom, prenom, telephone, sexe, salaire);
+        this.listeReservation = new ArrayList<>();
     }
 
     public List <Place> afficherPlaceDispo (Voiture voiture, LocalDate date){
@@ -22,13 +30,51 @@ public class Receptionniste extends Employee{
         }
         return placeDispo;
     }
+    //donner ticket ; // a besoi du methode estpayee de la partie 1
+    public Ticket donnerTicket (Reservation r) {
+        if (!r.estPayee()) {
+            System.out.println("Réservation non payé");
+            return null;
+        }
+        return new Ticket(
+                r.getId(),
+                r.getPlanDeVoyage().getDateDepart(),
+                r.getPlanDeVoyage().getDateArrive(),
+                r.getVoyageur(),
+                r.getPlace(),
+                r.getPlanDeVoyage().getVoiture()
+        );
+    }
+    //donner facture;
+    //besoin du prix de la reservation de la partie 1
 
-    public void enregistrerReservation (Reservation re){
-        listeReservation.add(re);
-
+    public  Facture donnerFacture (Reservation r){
+        if(r.getPrixTotal() <= 0){
+            System.out.println("Montant invalide");
+            return null;
+        }
+        return  new Facture(
+                r.getId(),
+                "Caisse-01",
+                r.getPrixTotal(),
+                r
+        );
 
     }
+    //enregsitrer reservation
+    public void enregistrerReservation (Reservation r){
+        listeReservation.add(r);
 
-    //donner ticket
-    //donner facture
+        HistoriqueAction log = new HistoriqueAction(
+                 r.getId(),
+                "Reservation fait pour" + r.getVoyageur().getNom(),
+                 LocalDateTime.now(),
+                 this
+        );
+    }
+
 }
+
+
+
+
